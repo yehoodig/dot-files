e4af25883b7d08c73664e5d645686c741a20645b
James McConnel <yehoodig@gmail.com>
2018-02-12 21:54:39 -0800

    Remove some emacs stuff

--- emacs/.emacs.d/elpa/sr-speedbar-20140914.2339/sr-speedbar-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "sr-speedbar" "20140914.2339" "Same frame speedbar" 'nil :commit "4f816528a32eb421197a768d6dcf3a05de83f642" :url "http://www.emacswiki.org/emacs/download/sr-speedbar.el" :keywords '("speedbar" "sr-speedbar.el"))
